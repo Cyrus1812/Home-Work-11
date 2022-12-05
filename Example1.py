b59bb574fdb9d4c335a90d8fadb16e06cf456a20
MAX <maksimdemidovru@mail.ru>
2022-12-05 21:13:50 +0300

    first commit

---
+++ Example1.py
@@ -0,0 +1,18 @@
+# Задача 1. Постройте график функции 𝑓(𝑥)=5𝑥^2+10𝑥−30
+# По графику определите, при каких значения x значение функции отрицательно.
+import matplotlib.pyplot as plt
+
+x = [x for x in range(-20, 20)]
+y = [5 * i**2 + 10 * i - 30 for i in x ]
+listX = []
+listY = []
+for i in range(len(y)):
+    if y[i] < 0:
+        listX.append(x[i])
+        listY.append(y[i])  
+plt.plot(x, y)
+plt.plot(listX, listY)           
+plt.title("Line graph")   
+plt.ylabel('Y axis')   
+plt.xlabel('X axis')   
+plt.show()
